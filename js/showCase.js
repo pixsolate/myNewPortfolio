bff022a81fc2528e48f4f57258f5954ed68c831f
1stwebstudio <81792465+pixsolate@users.noreply.github.com>
2021-06-14 14:42:05 -0700

    More Code fix and Converting to JavaScript repeated code

---
+++ js/showCase.js
@@ -0,0 +1,19 @@
+const showCase = `
+<div class="row">
+<div class="col-xl-6 col-md-6 text-left">
+  <a data-cursor-type="medium" class="allworks-link magnetize" href="contact.html" data-type="ajax-load" data-dist="1">Say Hello</a>
+</div>
+<div class="col-xl-6 col-md-6">
+  <div class="sliding-social">
+    <div class="follow-us">Follow me <i class="far fa-heart"></i></div>
+      <ul class="social">
+        <li class="magnetize" data-dist="1"><a data-cursor-type="medium" href="https://www.linkedin.com/in/federico-rodriguez-197b4182/">In</a></li>
+      </ul>
+  </div>
+</div>
+</div>
+`;
+
+const caseShow = document.querySelector(".showcase-bottom");
+
+caseShow.innerHTML = showCase;
